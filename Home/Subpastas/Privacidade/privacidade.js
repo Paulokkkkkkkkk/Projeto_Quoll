e2be90f1120eb69e800115662faa790577bca931
Paulo... <paulo666678886@gmail.com>
2025-11-03 10:57:16 -0300

    ..

--- Home/Subpastas/Privacidade/privacidade.js
+++ Home/Subpastas/Privacidade/privacidade.js
@@ -1,5 +1,5 @@
 document.getElementById("voltar-btn").addEventListener("click", function() {
-  window.location.href = "../index.html"; // ajusta o caminho conforme sua estrutura
+  window.location.href = "../../index.html"; // ajusta o caminho conforme sua estrutura
 });
 function voltarPagina() {
     window.history.back();
