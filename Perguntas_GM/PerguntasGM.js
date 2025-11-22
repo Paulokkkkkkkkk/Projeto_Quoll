aa536ba0ec8b6c06bddc8422c71a6f3131c26632
Paulo... <paulo666678886@gmail.com>
2025-11-22 10:36:10 -0300

    Games 1.0

--- Perguntas_GM/PerguntasGM.js
+++ Perguntas_GM/PerguntasGM.js
@@ -1,5 +1,100 @@
 const quizData = [
   {
+    question: "Qual o jogo mais famoso da franquia de Resident Evil?",
+    options: ["Resident Evil 4 (2005)", "Resident Evil 3: Nemesis (1999)", "Resident Evil 2 (Remake)", "Resident Evil 6 (2012)"],
+    answer: "Resident Evil 4 (2005)"
+  },
+    {
+    question: "Onde Kratos (God of War) nasceu?",
+    options: ["Sparta", "Atenas", "Rodes", "Tessalônica"],
+    answer: "Sparta"
+  },
+    {
+    question: "O que você obtêm quando derrota o Dragão End no Minecraft? ",
+    options: ["Cabeça do Dragão", "Ovo do Dragão", "Asas do Dragão", "Pack de Bedrock"],
+    answer: "Ovo do Dragão"
+  }, 
+    {
+    question: "Qual o jogo que contém mais parcerias atualmente?",
+    options: ["Brawl Stars", "Minecraft", "Fortnite", "Genshin Impact"],
+    answer: "Fortnite"
+  },
+      {
+    question: "Qual desses jogos é do gênero 'RPG'?",
+    options: ["Dispatch", "Hollow Knight", "Battlefield", "Deltarune"],
+    answer: "Deltarune"
+  },
+      {
+    question: "Qual o jogo mais popular da empresa 'Supercell'?",
+    options: ["Clash of Clans", "Clash Royale", "Brawl Stars", "Hay Day"],
+    answer: "Clash Royale"
+  },
+      {
+    question: "Qual o jogo mais jogado do roblox?",
+    options: ["Grow a Garden", "Blox Fruits", "Brookhaven RP", "Doors"],
+    answer: "Brookhaven RP"
+  },
+      {
+    question: "Em minecraft, qual item que o Enderman dropa quando morre?",
+    options: ["Enderpearl", "Bloco de Grama", "Cabeça de Enderman", "Ele Não Dropa"],
+    answer: "Enderpearl"
+  },
+      {
+    question: "Qual o jogo do gênero 'esporte' mais jogado atualmente?",
+    options: ["Fifa", "eFootball™", "NBA 2K", "Rocket League"],
+    answer: "Fifa"
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
+    question: "",
+    options: ["", "", "", ""],
+    answer: ""
+  },
+      {
     question: "",
     options: ["", "", "", ""],
     answer: ""
