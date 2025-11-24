7f172eeee4b2b11cce02a8d68792428746291024
joao-pedro-code-dev <donascimentoj628@gmail.com>
2025-11-24 11:04:57 -0300

    Perguntas_Conhecimentos_Gerais

--- Perguntas_Cg/perguntasCG.js
+++ Perguntas_Cg/perguntasCG.js
@@ -106,6 +106,63 @@ const quizData = [
   }
 ];
 
+
+
+
+
+function loadQuestion() {
+  const questionData = quizData[currentQuestion];
+  questionText.textContent = questionData.question;
+  questionNumber.textContent = `${currentQuestion + 1}.`;
+
+  optionsContainer.innerHTML = "";
+  questionData.options.forEach((opt) => {
+    const optionBtn = document.createElement("div");
+    optionBtn.classList.add("option");
+    optionBtn.textContent = opt;
+    optionBtn.addEventListener("click", () => selectOption(optionBtn, questionData.answer));
+    optionsContainer.appendChild(optionBtn);
+  });
+}
+
+function selectOption(selected, correctAnswer) {
+  const options = document.querySelectorAll(".option");
+  options.forEach(opt => {
+    opt.style.pointerEvents = "none";
+    if (opt.textContent === correctAnswer) {
+      opt.classList.add("correct");
+    }
+  });
+
+  if (selected.textContent === correctAnswer) {
+    score++;
+  } else {
+    selected.classList.add("incorrect");
+  }
+}
+
+nextBtn.addEventListener("click", () => {
+  currentQuestion++;
+  if (currentQuestion < quizData.length) {
+    loadQuestion();
+  } else {
+    showResult();
+  }
+});
+
+function showResult() {
+  document.querySelector(".question-header").classList.add("hidden");
+  optionsContainer.classList.add("hidden");
+  nextBtn.classList.add("hidden");
+  resultContainer.classList.remove("hidden");
+  resultContainer.innerHTML = `
+    <h2>Você acertou ${score} de ${quizData.length} perguntas!</h2>
+    <a href="../Home/index.html" class="botao-voltar">Voltar ao Menu</a>
+  `;
+}
+
+loadQuestion();
+
 // ---------------------- VARIÁVEIS E INICIALIZAÇÃO ----------------------
 
 // Função shuffle (randomizar)
